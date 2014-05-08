b691439a5c47ac67dbe9d228fe1c38d301df47b0
Miao Yu <miaoster@gmail.com>
2014-05-08 20:41:53 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shinyfeast::Application.config.secret_key_base = '9ede8d3a9a26ed959163993b9ff944f2fa15ea095af388eeee001a34b9f1857c4bfad994fdb22f41654d5edcfbe6245060bcec7efe7bec0dbe12b45c74905c70'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Shinyfeast::Application.config.session_store :cookie_store, key: '_shinyfeast_session'
